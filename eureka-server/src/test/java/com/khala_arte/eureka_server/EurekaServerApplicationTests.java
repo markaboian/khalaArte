87f3cfd24c9dd122a8604751603d1181b5c124b9
markaboian <abomark09@gmail.com>
2024-07-31 16:31:42 -0300

    starting project

---
+++ eureka-server/src/test/java/com/khala_arte/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khala_arte.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
